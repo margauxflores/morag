07e7a24c84fdcfaee8cfd19f2b5569750725dee7
Margaux Flores <margaux@tailor.tech>
2023-09-27 12:39:01 +0900

    refactor: remove unused imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,6 @@ import React from 'react';
 import '@/styles/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import {
-  CalendarIcon,
-  ChartPieIcon,
-  DocumentDuplicateIcon,
-  FolderIcon,
-  HomeIcon,
-  UsersIcon,
-} from '@heroicons/react/24/outline';
 import classNames from 'classnames';
 import { Sidebar } from '@/components/Sidebar';
 
@@ -19,15 +11,6 @@ export const metadata: Metadata = {
   title: 'Cruise Evaluation',
   description: 'Cruise Evaluation Survey Data',
 };
-
-const navigation = [
-  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-  { name: 'Team', href: '#', icon: UsersIcon, current: false },
-  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-  { name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
-  { name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
-];
 
 export default function RootLayout({
   children,
